57f6594ce300551bdfbbb6ae75b2e6de3a94b80c
tmr4 <42870032+tmr4@users.noreply.github.com>
2021-12-24 14:18:33 -0800

    Initialized IRQ_pin here

--- interrupts.py
+++ interrupts.py
@@ -3,6 +3,7 @@ from devices.acia65c51 import ACIA
 
 class Interrupts:
     def __init__(self, mon, mpu):
+        mpu.IRQ_pin = 1
         VIA(0x8800, mpu)
         ACIA(0x8400, mpu, mon)
         
